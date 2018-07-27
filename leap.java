e6141f533b901e5d387e2c85995ee3a1f5953b3f
sravyaoleti <41769652+sravyaoleti@users.noreply.github.com>
2018-07-27 12:03:47 +0530

    Create leap.java

---
+++ leap.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class leap
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=2016;
+		if(n%4==0)
+		{
+			System.out.println("yes");
+		}
+		else{
+			System.out.println("no");
+		}
+	}
+}
