21cc801a9557133e9833d4a1471fa52526c9b0c4
sravyaoleti <41769652+sravyaoleti@users.noreply.github.com>
2018-07-27 12:42:16 +0530

    Update Sumn.java

--- Sumn.java
+++ Sumn.java
@@ -5,7 +5,7 @@ class Sumn
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-	      int num=3,sum=0;
+	      int num=2,sum=0;
 	      for(int i=1;i<=num;i++)
                   {
                      sum+=i;
