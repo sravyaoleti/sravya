fefc0b1ad503457f7536b76f28e0b94408732a84
sravyaoleti <41769652+sravyaoleti@users.noreply.github.com>
2018-07-27 09:51:26 +0530

    Create evenodd.java

---
+++ evenodd.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+ class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int num=3;
+		if(num%2==0)
+		{
+			System.out.println("even number");
+		}
+		 else{
+			System.out.println("odd number");
+		}
+	}
+}
