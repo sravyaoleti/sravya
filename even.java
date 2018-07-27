f85661dab61b61457a26e8c28d08432537b0ddc5
sravyaoleti <41769652+sravyaoleti@users.noreply.github.com>
2018-07-27 09:58:58 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int num=3;
+		if(num%2==0)
+		{
+			System.out.println("odd number");
+		}
+		 else{
+			System.out.println("even number");
+		}
+	}
+}
