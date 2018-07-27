fad514a12fadeb19b882d8b4d73d5ee926e60b1b
sravyaoleti <41769652+sravyaoleti@users.noreply.github.com>
2018-07-27 11:03:49 +0530

    Create Vowel1.java

---
+++ Vowel1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Vowel
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		char ch='a';
+		if(ch =='a'|| ch =='e'|| ch=='i'|| ch=='o'|| ch=='u')
+		System.out.println("Vowel");
+		else
+		System.out.println("consonant");
+	}
+}
