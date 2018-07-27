27870ffb99ff5990f63baad8ea30fc7146c7ef79
sravyaoleti <41769652+sravyaoleti@users.noreply.github.com>
2018-07-27 12:23:00 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -5,8 +5,8 @@ class Hello
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		int i=2;
-		for(i=1;i<=2;i++)
+		int i=5;
+		for(i=1;i<=5;i++)
 		{
 			System.out.println("Hello");
 		}
