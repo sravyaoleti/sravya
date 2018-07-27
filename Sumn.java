71ebaed20f1753f780ba316829e4d74e9e1924a2
sravyaoleti <41769652+sravyaoleti@users.noreply.github.com>
2018-07-27 12:41:04 +0530

    Create Sumn.java

---
+++ Sumn.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Sumn
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	      int num=3,sum=0;
+	      for(int i=1;i<=num;i++)
+                  {
+                     sum+=i;
+                  }
+
+                  System.out.println(sum);
+            }
+}
