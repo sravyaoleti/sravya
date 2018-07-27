83b6943c7a8058f20be676c8eec23c1330b7c1bb
sravyaoleti <41769652+sravyaoleti@users.noreply.github.com>
2018-07-27 10:56:01 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class vowel
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		char ch='a';
+		if(ch =='a'|| ch =='e'|| ch=='i'|| ch=='o'|| ch=='u')
+		System.out.println("vowel");
+		else
+		System.out.println("consonant");
+	}
+}
