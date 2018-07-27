c9c51f745d9e41eeead6119d8c3673284a23a1c4
sravyaoleti <41769652+sravyaoleti@users.noreply.github.com>
2018-07-27 12:16:31 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Hello
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i=2;
+		for(i=1;i<=2;i++)
+		{
+			System.out.println("Hello");
+		}
+	}
+}
