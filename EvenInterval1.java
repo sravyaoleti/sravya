fe17627440fa1c2bd573dba6677b16a4b85dba25
sravyaoleti <41769652+sravyaoleti@users.noreply.github.com>
2018-07-28 09:26:13 +0530

    Update and rename EvenInterval.java to EvenInterval1.java

---
+++ EvenInterval1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class EvenInterval1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int e=1;
+		int f=10;
+		while ((0<e)&&(e<f-1))
+		{
+			 e = e + 1;
+			if (e % 2==0) 
+			{
+			   System.out.print(" "+e);
+	 }
+}
+	}
+}
