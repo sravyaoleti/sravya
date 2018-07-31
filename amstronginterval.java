d40578aea29878b17bf3e86c13a1f8e0cee751d1
sravyaoleti <41769652+sravyaoleti@users.noreply.github.com>
2018-07-31 12:38:33 +0530

    Create amstronginterval.java

---
+++ amstronginterval.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class amstronginterval
+{
+	
+	
+	 static void findArmstrong(int low, int high)
+    {
+        for (int i = low + 1; i < high; ++i) 
+        {
+            int x = i;
+            int n = 0;
+            while (x != 0) {
+                x /= 10;
+                ++n;
+            }
+            int pow_sum = 0; 
+            x = i;
+            while (x != 0) {
+                int digit = x % 10;
+                pow_sum += Math.pow(digit, n);
+                x /= 10;
+            }
+            if (pow_sum == i) 
+                System.out.print(i + " ");     
+        }
+    }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 int num1 = 369;
+        int num2 = 371;
+        findArmstrong(num1, num2);
+        System.out.println();
+	}
+}
