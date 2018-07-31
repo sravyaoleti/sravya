711ca74804035641e8d378b7e5523e2420780d4b
sravyaoleti <41769652+sravyaoleti@users.noreply.github.com>
2018-07-31 12:16:10 +0530

    Create amstrong.java

---
+++ amstrong.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class amstrong
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int c=0,a,temp;  
+    int n=371;//It is the number to check armstrong  
+    temp=n;  
+    while(n>0)  
+    {  
+    a=n%10;  
+    n=n/10;  
+    c=c+(a*a*a);  
+    }  
+    if(temp==c) 
+    {
+    System.out.println("yes");   
+    }
+    else{  
+        System.out.println("no");  
+    }
+	}
+}
