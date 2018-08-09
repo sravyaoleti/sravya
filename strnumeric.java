5849701716b3542df485d9949ed9cec9a114fd16
sravyaoleti <41769652+sravyaoleti@users.noreply.github.com>
2018-08-09 19:30:52 +0530

    Create strnumeric.java

---
+++ strnumeric.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 
+       char d;
+       Scanner s=new Scanner(System.in);
+       d=s.next().charAt(0);
+       if(d>='0'&&d<='9'){
+           System.out.println("Yes");
+       }
+      else{
+           System.out.println("No");
+      }
+	}
+}
