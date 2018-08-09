b830d797c3b8c47bd8736136e85d5922f2d0b446
sravyaoleti <41769652+sravyaoleti@users.noreply.github.com>
2018-08-09 19:46:42 +0530

    Create minutes.java

---
+++ minutes.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+System.out.println("enter the time in minutes :");
+int min=sc.nextInt();
+int hr=0,min1=0;
+if(min>0)
+{
+if(min<60)
+{
+	min1=min;
+}
+else if(min>60)
+{
+	hr=min/60;
+	min1=min%60;
+}
+System.out.println(hr+":"+min1);
+}
+else
+{
+System.out.println("invalid input\n");
+}
+	}
+}
