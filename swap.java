c387929d4d06ecdeb990d9ad1154a158ce9e0db8
sravyaoleti <41769652+sravyaoleti@users.noreply.github.com>
2018-08-09 16:45:18 +0530

    Create swap.java

---
+++ swap.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a;
+     int b;
+     int t;
+     Scanner s=new Scanner(System.in);
+     a=s.nextInt();
+     b=s.nextInt();
+      t=a;
+     a=b;
+     b=t;
+     System.out.printf("%d %d",a,b);
+
+		
+	}
+}
