44fddbc0e3045950b119c290af84a9253427699c
sravyaoleti <41769652+sravyaoleti@users.noreply.github.com>
2018-08-11 10:41:56 +0530

    Update positive.java

--- positive.java
+++ positive.java
@@ -7,7 +7,9 @@ class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		int num=3;
+		int num;
+		Scanner s=new Scanner(System.in);
+		num=s.nextInt();
 		if(num%2==0)
 		{
 			System.out.println("odd");
