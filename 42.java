1af477f870cce64689218d9732b7fbfdbd9fcfb3
sravyaoleti <41769652+sravyaoleti@users.noreply.github.com>
2018-08-11 12:45:44 +0530

    Create 42.java

---
+++ 42.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  String s1,s2;	
+      Scanner sc=new Scanner(System.in);
+      s1=sc.next();
+      s2=sc.next();
+      if(s1.length()>s2.length())
+      {
+       System.out.println(s1);
+      }
+      else if(s1.length()==s2.length())
+      {
+        System.out.println(s2);
+       }
+     else
+       System.out.println("invalid");
+	}
+}
