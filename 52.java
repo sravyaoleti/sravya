ed4b166321f607808a7ceba5debf48cccf1016b5
sravyaoleti <41769652+sravyaoleti@users.noreply.github.com>
2018-08-14 11:48:36 +0530

    Create 52.java

---
+++ 52.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a[]={1,2,3,4,5,6,7,8,9,10};
+     String c[]={"one","two","three","four","five","six","seven","eight","nine","ten"};
+     int n;
+     Scanner s=new Scanner(System.in);
+     n=s.nextInt();
+     
+     for(int i=0;i<a.length;i++){
+         if(a[i]==n){
+             System.out.println(c[i]);
+         }
+	}
+}
+}
