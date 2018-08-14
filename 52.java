01a7fa73025e9efd2ae8dcaf1479dba69d66e6c8
sravyaoleti <41769652+sravyaoleti@users.noreply.github.com>
2018-08-14 11:50:37 +0530

    Update 52.java

--- 52.java
+++ 52.java
@@ -8,7 +8,7 @@ class Ideone
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		int a[]={1,2,3,4,5,6,7,8,9,10};
-     String c[]={"one","two","three","four","five","six","seven","eight","nine","ten"};
+     String c[]={"One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten"};
      int n;
      Scanner s=new Scanner(System.in);
      n=s.nextInt();
