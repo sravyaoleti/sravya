9791f29899e439032600bbfbdaef1aa26a8a66c3
sravyaoleti <41769652+sravyaoleti@users.noreply.github.com>
2018-08-14 12:42:48 +0530

    Update 58.java

--- 58.java
+++ 58.java
@@ -1,18 +1,13 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
 class exits
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
 		Scanner s=new Scanner(System.in);
-		
 		int n,k;
 		int a[]=new int[20] ;
-		
 		n=s.nextInt();
 		k=s.nextInt();
 		int count=0;
@@ -33,5 +28,5 @@ class exits
 			System.out.println("no");
 		
 		
-	}
+	 }
 }
