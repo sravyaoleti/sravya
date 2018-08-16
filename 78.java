2830b6711d01ab3fab29dc5e9386eb4f1dbfa694
sravyaoleti <41769652+sravyaoleti@users.noreply.github.com>
2018-08-16 11:17:45 +0530

    Create 78.java

---
+++ 78.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		if(n%13==0)
+		System.out.print("yes");
+		else
+		System.out.print("no");
+	}
+}
