528f45b324fbad80b014f6a58738f70669a14ad4
sravyaoleti <41769652+sravyaoleti@users.noreply.github.com>
2018-08-17 12:29:56 +0530

    Create 84.java

---
+++ 84.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+     Scanner s=new Scanner(System.in);
+     char ch=s.next().charAt(0);
+     System.out.println((int)ch);
+	}
+}
