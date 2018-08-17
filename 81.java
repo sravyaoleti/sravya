553f5c1235f66c5bf88285a227bc412d381b0bc1
sravyaoleti <41769652+sravyaoleti@users.noreply.github.com>
2018-08-17 09:33:51 +0530

    Create 81.java

---
+++ 81.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner in=new Scanner(System.in);
+		int a=in.nextInt();
+		int b=in.nextInt();
+		int r=Math.abs(a-b);
+		System.out.print(r);
+	}
+}
