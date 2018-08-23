f3f3226606d39d4c8dbd3dc8327ada31a1b91399
sravyaoleti <41769652+sravyaoleti@users.noreply.github.com>
2018-08-23 10:51:53 +0530

    Create 93.java

---
+++ 93.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner in=new Scanner(System.in);
+	 int temp=in.nextInt();
+           int  kelvin=273;
+	 kelvin=kelvin+temp;
+	 System.out.println(kelvin);
+
+	}
+}
