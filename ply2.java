84eaaf35b750836c719364e29748df1f7447530e
sravyaoleti <41769652+sravyaoleti@users.noreply.github.com>
2018-08-29 11:39:00 +0530

    Create ply2.java

---
+++ ply2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int n1=1;
+        for(int i=1;i<=n;i++)
+        {
+            n1=n1*i;
+        }
+        System.out.print(n1);
+	}
+}
